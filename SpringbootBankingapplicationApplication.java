edc92599ba80045032ce751a88434d6022e0f1f7
pranjalipolstya <168225956+pranjalipolstya@users.noreply.github.com>
2025-06-21 00:51:45 +0530

    Add files via upload

---
+++ SpringbootBankingapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.applicationbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBankingapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBankingapplicationApplication.class, args);
+	}
+
+}
